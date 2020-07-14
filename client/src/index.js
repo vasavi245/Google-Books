96a58d6c2e37fb1df29752a8b580cae70656ddd7
vasavi245 <r.vasavi04@gmail.com>
2020-07-14 20:15:23 +0800

    removed materialize

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import 'materialize-css/dist/js/materialize.min.js';
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
