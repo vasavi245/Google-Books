a305614d4273b056deab4633ff0795d6be7db87d
vasavi245 <r.vasavi04@gmail.com>
2020-07-14 20:40:02 +0800

    changes to server.js

--- server.js
+++ server.js
@@ -13,13 +13,20 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gogglebooks");
+
+
 // Define API routes here
 app.use(routes);
 
-// Send every other request to the React app
+// Send every request to the React app
 // Define any API routes before this runs
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gogglebooks");
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
