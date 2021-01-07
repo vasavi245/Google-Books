72b2fdce793eb06c2b6114ac561933f809cd8871
vasavi245 <r.vasavi04@gmail.com>
2021-01-07 08:42:01 +0800

    mongo connection string

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
@@ -13,8 +14,8 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gogglebooks");
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+console.log(process.env.MONGODB_URI);
 // Define API routes here
 app.use(routes);
 
